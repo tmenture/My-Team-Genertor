8e7a9fe59bf47a971338bc9a0b210070ca43239a
tmenture <thomasoxemail@gmail.com>
2022-05-07 02:46:42 -0400

    finished html template

--- src/createHTML.js
+++ src/createHTML.js
@@ -16,7 +16,7 @@ const createManager = function (manager) {
         </div>
     </div>
     `;
-}
+};
 
 // Creates Engineer section
 const createEngineer = function (engineer) {
@@ -36,7 +36,7 @@ const createEngineer = function (engineer) {
         </div>
     </div>
     `;
-}
+};
 
 // Creates Intern section
 const createIntern = function (intern) {
@@ -56,4 +56,82 @@ const createIntern = function (intern) {
         </div>
     </div>
     `;
-}
+};
+
+// array data gets pushed to
+createHTML = (data) => {
+    pageArray = [];
+
+    for (let i = 0; i < data.length; i++) {
+        const employee = data[i];
+        const role = employee.getRole();
+
+        // runs manager function
+        if (role === 'Manager') {
+            const managerSection = createManager(employee);
+
+            pageArray.push(managerSection);
+        }
+
+        // runs engineer function
+        if (role === 'Engineer') {
+            const engineerSection = createEngineer(employee);
+
+            pageArray.push(engineerSection);
+        }
+
+        // runs intern function
+        if (role === 'Intern') {
+            const internSection = createIntern(employee);
+
+            pageArray.push(internSection);
+        }
+
+    }
+
+    // joins strings
+    const employeeSections = pageArray.join('');
+
+    // returns to generated HTML
+    const createTeam = createTeamPage(employeeSections);
+    return createTeam;
+};
+
+const createTeamPage = function (employeeSections) {
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Team Profile</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+        <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <header>
+            <nav class="navbar" id="navbar">
+                <span class="navbar-brand mb-0 h1 w-100 text-center" id="navbar-text">Team Profile</span>
+            </nav>
+        </header>
+        <main>
+            <div class="container">
+                <div class="row justify-content-center" id="team-cards">
+                    <!--Team Cards-->
+                    ${employeeSections}
+                </div>
+            </div>
+        </main>
+
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    </body>
+    </html>
+    `;
+};
+
+module.exports = createHTML;
